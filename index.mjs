701247b3d03b2fbb8c2f6f668d465871f9d6c9e9
Shiva-Prasad-Reddy-K <shivaprasad14083@gmail.com>
2025-08-17 02:22:51 +0530

    Added the done functionality to the project

--- index.mjs
+++ index.mjs
@@ -1,4 +1,5 @@
 import { Command } from "commander";
+import chalk from "chalk";
 import fs from 'fs';
 
 const program = new Command();
@@ -9,19 +10,28 @@ program.command('add')
     .argument('<string>', "Task to add")
     .action((str) => {
         contents.push({task: str, status: true})
-        console.log(contents)
         fs.writeFileSync("tasks.txt", JSON.stringify(contents), {encoding: 'utf8'})
+        console.log(chalk.green.bold('Task added to the list'))
+    })
+
+program.command('done')
+    .description('Mark a task as done in the list')
+    .argument('<string>', "Task to mark as done")
+    .action((str) => {
+        let found = false;
+        for(let i = 0; i < contents.length; i++) {
+            if (str === contents[i].task) {
+                contents[i].status = false;
+                found = true;
+                console.log(chalk.green('Task marked as done'))
+                break;
+            }
+        }
+        if(!found) {
+            console.log(chalk.red("Ther is no such task"))
+        }
     })
 
 
-program.command('list')
-    .description('List all the tasks in the todo list')
-    .option('-p, --pending', "list only the pending tasks")
-    .option('--all', "list all the tasks for the day")
-    .action((options) => {
-        for(let i = 0; i < contents.length; i++){
-            console.log(contents[i].task)
-        }
-    });
 
 program.parse();
